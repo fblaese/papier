0262d2af0c3e57bf01741399fce4ad3404519e37
Fabian Bläse <fabian@blaese.de>
2018-10-06 03:33:35 +0200

    tiles

--- papier.java
+++ papier.java
@@ -13,12 +13,14 @@ class ImageWriter implements Runnable {
 	private int height;
 	private int from;
 	private int to;
+	private int tox;
+	private int fromx;
 
 	private Writer out;
 
 	private Random rand;
 
-	ImageWriter(String server, int port, BufferedImage img, int from, int to) {
+	ImageWriter(String server, int port, BufferedImage img, int from, int to, int fromx, int tox) {
 		try {
 			connect(server, port);
 		} catch (Exception e) {
@@ -30,6 +32,8 @@ class ImageWriter implements Runnable {
 		this.height = img.getHeight();
 		this.from = from;
 		this.to = to;
+		this.fromx = fromx;
+		this.tox = tox;
 
 		this.out = out;
 		this.rand = new Random();
@@ -56,9 +60,11 @@ class ImageWriter implements Runnable {
 		int x;
 		int y;
 
+		System.out.println("thread: " + from + " " + fromx);
+
 		while (true) {
 			for (y = from; y < to; y++) {
-				for (x = 0; x < width; x++) {
+				for (x = fromx; x < tox; x++) {
 					try {
 						out.write("PX " + x + " " + y + " " + rgbToHex(img.getRGB(x, y)) + "\n");
 					} catch (Exception e) {}
@@ -80,16 +86,22 @@ public class papier {
 		int port = Integer.valueOf(args[1]);
 		BufferedImage image = ImageIO.read(new File(args[2]));
 		int height = image.getHeight();
+		int width = image.getWidth();
 		int threads = Integer.valueOf(args[3]);
 
 		Thread t[] = new Thread[threads];
 
-		int zone = height/threads;
+		int sqrtthreads = (int) Math.sqrt(threads);
+		int zoney = height/sqrtthreads;
+		int zonex = width/sqrtthreads;
 
-		for (int i = 0; i < threads; i++) {
-			ImageWriter writer = new ImageWriter(server, port, image, i*zone, (i+1)*zone);
-			t[i] = new Thread(writer);
-			t[i].start();
+		for (int i = 0; i < sqrtthreads; i++) {
+			for (int j = 0; j < sqrtthreads; j++) {
+				System.out.println("Starting Writer. from (" + j * zonex + "," + i * zoney + ") to (" + (j+1) * zonex + "," + (i+1)*zoney + ")");
+				ImageWriter writer = new ImageWriter(server, port, image, i*zoney, (i+1)*zoney, j*zonex, (j+1)*zonex);
+				t[i+j] = new Thread(writer);
+				t[i+j].start();
+			}
 		}
 
 		for (int i = 0; i < threads; i++) {
